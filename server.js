37c425b10a0f5b89b19b5f57bd2d01a2a6f8ce59
Will Simmons <willsimmons@gmail.com>
2015-04-12 16:39:58 -0700

    other middleware for server

--- server.js
+++ server.js
@@ -4,12 +4,14 @@ var morgan         = require('morgan');
 var bodyParser     = require('body-parser');
 var methodOverride = require('method-override');
 var app            = express();
-
+var db             = require('./model/db'); //db.js will hold our user mongo db data
+var bcrypt = require('bcrypt'); //for salting user passwords
 app.use(express.static(__dirname + '/public'));     // set the static files location /public/img will be /img for users
 app.use(morgan('dev'));                     // log every request to the console
 app.use(bodyParser.urlencoded({ extended: false }));    // parse application/x-www-form-urlencoded
 app.use(bodyParser.json());    // parse application/json
 app.use(methodOverride());                  // simulate DELETE and PUT
+app.set('view engine', 'ejs');  //for ejs views vs plain html,  
 //end server setup
 
 
